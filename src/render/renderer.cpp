baa681ca77d3d1e8bd9e346a1fc08aef41211b11
caioguedesam <caioguedesam@gmail.com>
2022-12-22 20:15:28 -0300

    Moving OBJ loading into function

--- src/render/renderer.cpp
+++ src/render/renderer.cpp
@@ -50,6 +50,12 @@ u32 sponzaVAO;
 u32 sponzaVBO;
 u32 sponzaEBO;
 
+Array<MeshVertex> bunnyVertices;
+Array<u32> bunnyIndices;
+u32 bunnyVAO;
+u32 bunnyVBO;
+u32 bunnyEBO;
+
 m4f modelMatrix = {};
 m4f viewMatrix = {};
 m4f projMatrix = {};
@@ -59,6 +65,119 @@ GLMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsize
         const GLchar* message, const void* userParam)
 {
     ASSERTF(type != GL_DEBUG_TYPE_ERROR, "[OPENGL ERROR]: %s", message);
+}
+
+void LoadOBJModel(FilePath assetPath, Array<MeshVertex>* outVertices, Array<u32>* outIndices)
+{
+    ASSERT(outVertices && outIndices);
+
+    fastObjMesh* objData = fast_obj_read(assetPath.str.ToCStr());
+
+    *outVertices = ArrayAlloc<MeshVertex>(
+            &memArena_Mesh,
+            objData->index_count * 3
+            );
+    *outIndices = ArrayAlloc<u32>(
+            &memArena_Mesh,
+            objData->index_count * 3
+            );
+
+    u64 currentIndex = 0;
+    for(u64 g = 0; g < objData->group_count; g++)
+    {
+        u64 groupFaceCount = objData->groups[g].face_count;
+        u64 groupFaceOffset = objData->groups[g].face_offset;
+        u64 groupIndexOffset = objData->groups[g].index_offset;
+
+        u64 currentFaceOffset = 0;  // Cursor that always points to current face (this is needed because
+                                    // faces can have more than 3 sides).
+
+        for(u64 f = groupFaceOffset; f < groupFaceOffset + groupFaceCount; f++)
+        {
+            if(objData->face_vertices[f] == 3)
+            {
+                for(u64 v = 0; v < 3; v++)
+                {
+                    u64 i = (currentFaceOffset + v) + groupIndexOffset;
+                    u64 i_position = objData->indices[i].p;
+                    ASSERT(i_position);
+                    u64 i_normal = objData->indices[i].n;
+                    ASSERT(i_normal);
+                    u64 i_texcoord = objData->indices[i].t;
+                    ASSERT(i_texcoord);
+
+                    v3f vertexPos =
+                    {
+                        objData->positions[i_position * 3 + 0],
+                        objData->positions[i_position * 3 + 1],
+                        objData->positions[i_position * 3 + 2],
+                    };
+
+                    v3f vertexNormal =
+                    {
+                        objData->normals[i_normal * 3 + 0],
+                        objData->normals[i_normal * 3 + 1],
+                        objData->normals[i_normal * 3 + 2],
+                    };
+                    
+                    v2f vertexTexcoord =
+                    {
+                        objData->texcoords[i_texcoord * 2 + 0],
+                        objData->texcoords[i_texcoord * 2 + 1],
+                    };
+
+                    outVertices->Push({vertexPos, vertexNormal, vertexTexcoord});
+                    outIndices->Push(currentIndex++);
+                }
+            }
+            else
+            {
+                // For non-triangle faces, triangulate then parse
+                // N sided polygon has N-2 triangles
+                for(u64 point = 1; point < objData->face_vertices[f] - 1; point++)
+                {
+                    u64 triIndices[] = {0, point, point + 1};
+                    for(u64 v = 0; v < 3; v++)
+                    {
+                        u64 i = (currentFaceOffset + triIndices[v]) + groupIndexOffset;
+
+                        u64 i_position = objData->indices[i].p;
+                        ASSERT(i_position);
+                        u64 i_normal = objData->indices[i].n;
+                        ASSERT(i_normal);
+                        u64 i_texcoord = objData->indices[i].t;
+                        ASSERT(i_texcoord);
+
+                        v3f vertexPos =
+                        {
+                            objData->positions[i_position * 3 + 0],
+                            objData->positions[i_position * 3 + 1],
+                            objData->positions[i_position * 3 + 2],
+                        };
+
+                        v3f vertexNormal =
+                        {
+                            objData->normals[i_normal * 3 + 0],
+                            objData->normals[i_normal * 3 + 1],
+                            objData->normals[i_normal * 3 + 2],
+                        };
+                        
+                        v2f vertexTexcoord =
+                        {
+                            objData->texcoords[i_texcoord * 2 + 0],
+                            objData->texcoords[i_texcoord * 2 + 1],
+                        };
+
+                        outVertices->Push({vertexPos, vertexNormal, vertexTexcoord});
+                        outIndices->Push(currentIndex++);
+                    }
+                }
+            }
+            currentFaceOffset += objData->face_vertices[f];
+        }
+    }
+
+    fast_obj_destroy(objData);
 }
 
 void InitRenderer(Window* window)
@@ -79,7 +198,7 @@ void InitRenderer(Window* window)
     // Resource memory
     MemArenaInit(&memArena_Shader, KB(256));
     MemArenaInit(&memArena_Texture, MB(1));
-    MemArenaInit(&memArena_Mesh, MB(200));
+    MemArenaInit(&memArena_Mesh, MB(256));
 
     // Shader resources
     // TODO(caio)#RENDER: Change this when starting support for multiple shaders + hot reload
@@ -168,116 +287,10 @@ void InitRenderer(Window* window)
     glGenerateMipmap(GL_TEXTURE_2D);
 
     // Loading OBJ model
-    fastObjMesh* objData = fast_obj_read(MODELS_PATH"sponza/sponza.obj");
+    LoadOBJModel(MakePath(MODELS_PATH"sponza/sponza.obj"), &sponzaVertices, &sponzaIndices);
+    LoadOBJModel(MakePath(MODELS_PATH"bunny/bunny.obj"), &bunnyVertices, &bunnyIndices);
 
-    sponzaVertices = ArrayAlloc<MeshVertex>(
-            &memArena_Mesh,
-            objData->index_count * 3
-            );
-    sponzaIndices = ArrayAlloc<u32>(
-            &memArena_Mesh,
-            objData->index_count * 3
-            );
-
-    u64 currentIndex = 0;
-    for(u64 g = 0; g < objData->group_count; g++)
-    {
-        u64 groupFaceCount = objData->groups[g].face_count;
-        u64 groupFaceOffset = objData->groups[g].face_offset;
-        u64 groupIndexOffset = objData->groups[g].index_offset;
-
-        u64 currentFaceOffset = 0;  // Cursor that always points to current face (this is needed because
-                                    // faces can have more than 3 sides).
-
-        for(u64 f = groupFaceOffset; f < groupFaceOffset + groupFaceCount; f++)
-        {
-            if(objData->face_vertices[f] == 3)
-            {
-                for(u64 v = 0; v < 3; v++)
-                {
-                    //u64 i = (f * 3 + v) + groupIndexOffset;
-                    u64 i = (currentFaceOffset + v) + groupIndexOffset;
-                    u64 i_position = objData->indices[i].p;
-                    ASSERT(i_position);
-                    u64 i_normal = objData->indices[i].n;
-                    ASSERT(i_normal);
-                    u64 i_texcoord = objData->indices[i].t;
-                    ASSERT(i_texcoord);
-
-                    v3f vertexPos =
-                    {
-                        objData->positions[i_position * 3 + 0],
-                        objData->positions[i_position * 3 + 1],
-                        objData->positions[i_position * 3 + 2],
-                    };
-
-                    v3f vertexNormal =
-                    {
-                        objData->normals[i_normal * 3 + 0],
-                        objData->normals[i_normal * 3 + 1],
-                        objData->normals[i_normal * 3 + 2],
-                    };
-                    
-                    v2f vertexTexcoord =
-                    {
-                        objData->texcoords[i_texcoord * 2 + 0],
-                        objData->texcoords[i_texcoord * 2 + 1],
-                    };
-
-                    sponzaVertices.Push({vertexPos, vertexNormal, vertexTexcoord});
-                    sponzaIndices.Push(currentIndex++);
-                }
-            }
-            else
-            {
-                // For non-triangle faces, triangulate then parse
-                // N sided polygon has N-2 triangles
-                for(u64 point = 1; point < objData->face_vertices[f] - 1; point++)
-                {
-                    u64 triIndices[] = {0, point, point + 1};
-                    for(u64 v = 0; v < 3; v++)
-                    {
-                        u64 i = (currentFaceOffset + triIndices[v]) + groupIndexOffset;
-
-                        u64 i_position = objData->indices[i].p;
-                        ASSERT(i_position);
-                        u64 i_normal = objData->indices[i].n;
-                        ASSERT(i_normal);
-                        u64 i_texcoord = objData->indices[i].t;
-                        ASSERT(i_texcoord);
-
-                        v3f vertexPos =
-                        {
-                            objData->positions[i_position * 3 + 0],
-                            objData->positions[i_position * 3 + 1],
-                            objData->positions[i_position * 3 + 2],
-                        };
-
-                        v3f vertexNormal =
-                        {
-                            objData->normals[i_normal * 3 + 0],
-                            objData->normals[i_normal * 3 + 1],
-                            objData->normals[i_normal * 3 + 2],
-                        };
-                        
-                        v2f vertexTexcoord =
-                        {
-                            objData->texcoords[i_texcoord * 2 + 0],
-                            objData->texcoords[i_texcoord * 2 + 1],
-                        };
-
-                        sponzaVertices.Push({vertexPos, vertexNormal, vertexTexcoord});
-                        sponzaIndices.Push(currentIndex++);
-                    }
-                }
-            }
-            currentFaceOffset += objData->face_vertices[f];
-        }
-    }
-
-    fast_obj_destroy(objData);
-
-    // Creating OpenGL structures for loaded model
+    // Creating OpenGL structures for sponza model
     glGenVertexArrays(1, &sponzaVAO);
     glGenBuffers(1, &sponzaVBO);
     glGenBuffers(1, &sponzaEBO);
@@ -290,10 +303,25 @@ void InitRenderer(Window* window)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sponzaEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sponzaIndices.Size(), sponzaIndices.data, GL_STATIC_DRAW);
 
-    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (void*)0);
-    //glEnableVertexAttribArray(0);
-    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (void*)(3 * sizeof(f32)));
-    //glEnableVertexAttribArray(1);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(MeshVertex), (void*)StructOffset(MeshVertex, position));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(MeshVertex), (void*)StructOffset(MeshVertex, normal));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(MeshVertex), (void*)StructOffset(MeshVertex, texcoord));
+    glEnableVertexAttribArray(2);
+
+    // Creating OpenGL structures for bunny model
+    glGenVertexArrays(1, &bunnyVAO);
+    glGenBuffers(1, &bunnyVBO);
+    glGenBuffers(1, &bunnyEBO);
+
+    glBindVertexArray(bunnyVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, bunnyVBO);
+    glBufferData(GL_ARRAY_BUFFER, bunnyVertices.Size(), bunnyVertices.data, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bunnyEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, bunnyIndices.Size(), bunnyIndices.data, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(MeshVertex), (void*)StructOffset(MeshVertex, position));
     glEnableVertexAttribArray(0);
@@ -303,9 +331,6 @@ void InitRenderer(Window* window)
     glEnableVertexAttribArray(2);
 
     // Temporary transformation matrices
-    modelMatrix = ScaleMatrix({0.01f, 0.01f, 0.01f});
-    modelMatrix = RotationMatrix(TO_RAD(90.f), {0.f, 1.f, 0.f}) * modelMatrix;
-
     v3f cameraCenter = {0,2,3};
     v3f cameraDir = {0,0,-1};
     f32 cameraFov = 45.f;
@@ -330,6 +355,9 @@ void RenderFrame()
     glClearColor(1.f, 0.5f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    modelMatrix = ScaleMatrix({0.01f, 0.01f, 0.01f});
+    modelMatrix = RotationMatrix(TO_RAD(90.f), {0.f, 1.f, 0.f}) * modelMatrix;
+
     // TODO(caio)#RENDER: This is temporary code before setting up proper object drawing.
     glUseProgram(shaderProgramHandle);
     //glBindTexture(GL_TEXTURE_2D, textureHandle);
@@ -344,6 +372,19 @@ void RenderFrame()
     glUniformMatrix4fv(location, 1, GL_TRUE, &projMatrix.m00);
 
     glDrawElements(GL_TRIANGLES, sponzaIndices.count, GL_UNSIGNED_INT, 0);
+
+    modelMatrix = Identity();
+
+    glBindVertexArray(bunnyVAO);
+
+    location = glGetUniformLocation(shaderProgramHandle, "u_Model");
+    glUniformMatrix4fv(location, 1, GL_TRUE, &modelMatrix.m00);
+    location = glGetUniformLocation(shaderProgramHandle, "u_View");
+    glUniformMatrix4fv(location, 1, GL_TRUE, &viewMatrix.m00);
+    location = glGetUniformLocation(shaderProgramHandle, "u_Proj");
+    glUniformMatrix4fv(location, 1, GL_TRUE, &projMatrix.m00);
+
+    glDrawElements(GL_TRIANGLES, bunnyIndices.count, GL_UNSIGNED_INT, 0);
 }
 
 } // namespace Ty
